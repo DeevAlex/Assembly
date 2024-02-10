a4aa25d6567c62af1b1fec5587971b93087c2baf
Alex de Matos Caceres <101301836+DeevAlex@users.noreply.github.com>
2024-02-10 16:43:41 -0300

    Delete AssemblyMIPS directory

--- AssemblyMIPS/imprimirInteiro.asm
+++
@@ -1,8 +0,0 @@
-.data  # (não é preciso) armazena as variaveis que venham da memoria ram
-	idade: .word 56 # valor inteiro na memoria ram
-
-.text # obrigatorio
-
-	li $v0, 1 # imprimindo
-	lw $a0, idade # pegando a variavel na memoria ram 
-	syscall 
